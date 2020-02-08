56fc3bad05b1fdf3f334e4fd624ced77f4f16975
王锦光 <wangjg@changhong.com>
2020-02-08 12:40:45 +0800

    通过数据角色Id获取数据权限类型,判断应用模块Id是否输入VO：VERSION = 6.0.1.16

--- sei-basic-service/src/main/java/com/changhong/sei/basic/controller/DataAuthorizeTypeController.java
+++ sei-basic-service/src/main/java/com/changhong/sei/basic/controller/DataAuthorizeTypeController.java
@@ -9,6 +9,7 @@ import com.changhong.sei.core.controller.DefaultBaseEntityController;
 import com.changhong.sei.core.dto.ResultData;
 import com.changhong.sei.core.service.BaseEntityService;
 import io.swagger.annotations.Api;
+import org.apache.commons.lang3.StringUtils;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.PropertyMap;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -126,12 +127,16 @@ public class DataAuthorizeTypeController implements DefaultBaseEntityController<
     /**
      * 通过数据角色Id获取数据权限类型（VO）
      *
-     * @param appModuleId
+     * @param appModuleId 应用模块Id
      * @param roleId      数据角色Id
      * @return 数据权限类型
      */
     @Override
     public ResultData<List<DataAuthorizeTypeVo>> getByAppModuleAndDataRole(String appModuleId, String roleId) {
+        // 判断应用模块Id是否输入
+        if (StringUtils.isBlank(appModuleId)){
+            return getByDataRole(roleId);
+        }
         return ResultData.success(service.getByAppModuleAndDataRole(appModuleId, roleId));
     }
 
