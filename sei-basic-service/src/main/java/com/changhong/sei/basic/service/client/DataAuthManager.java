c153e146d3906cddcb0d59f02d6d45e433684670
王锦光 <wangjg@changhong.com>
2020-05-10 22:05:49 +0800

    实现数据权限服务方法的管理器,如果是公司和组织机构直接执行业务逻辑：:VERSION = 6.0.1.107

--- sei-basic-service/src/main/java/com/changhong/sei/basic/service/client/DataAuthManager.java
+++ sei-basic-service/src/main/java/com/changhong/sei/basic/service/client/DataAuthManager.java
@@ -1,6 +1,8 @@
 package com.changhong.sei.basic.service.client;
 
 import com.changhong.sei.apitemplate.ApiTemplate;
+import com.changhong.sei.basic.service.CorporationService;
+import com.changhong.sei.basic.service.OrganizationService;
 import com.changhong.sei.core.dto.ResultData;
 import com.changhong.sei.core.dto.auth.AuthEntityData;
 import com.changhong.sei.core.dto.auth.AuthTreeEntityData;
@@ -20,12 +22,18 @@ import java.util.List;
  */
 @Component
 public class DataAuthManager {
-    @Autowired
-    private ApiTemplate apiTemplate;
     private static final String GET_AUTH_ENTITY_DATA_METHOD = "getAuthEntityDataByIds";
     private static final String GET_AUTH_TREE_ENTITY_DATA_METHOD = "getAuthTreeEntityDataByIds";
     private static final String FIND_ALL_AUTH_ENTITY_DATA_METHOD = "findAllAuthEntityData";
     private static final String FIND_ALL_AUTH_TREE_ENTITY_DATA_METHOD = "findAllAuthTreeEntityData";
+    private static final String API_ORGANIZATION = "organization";
+    private static final String API_CORPORATION = "corporation";
+    @Autowired
+    private ApiTemplate apiTemplate;
+    @Autowired
+    private CorporationService corporationService;
+    @Autowired
+    private OrganizationService organizationService;
 
     /**
      * 通过业务实体Id清单获取数据权限值清单
@@ -35,6 +43,10 @@ public class DataAuthManager {
      * @return 数据权限值清单
      */
     public List<AuthEntityData> getAuthEntityDataByIds(String appModuleCode, String apiPath, List<String> entityIds){
+        // 判断是公司,直接执行公司业务逻辑
+        if (apiPath.equals(API_CORPORATION)) {
+            return corporationService.getAuthEntityDataByIds(entityIds);
+        }
         //调用API服务，获取业务实体
         String path = String.format("%s/%s", apiPath, GET_AUTH_ENTITY_DATA_METHOD);
         ParameterizedTypeReference<ResultData<List<AuthEntityData>>> typeReference = new ParameterizedTypeReference<ResultData<List<AuthEntityData>>>() {};
@@ -52,6 +64,10 @@ public class DataAuthManager {
      * @return 数据权限值清单
      */
     public List<AuthEntityData> findAllAuthEntityData(String appModuleCode, String apiPath){
+        // 判断是公司,直接执行公司业务逻辑
+        if (apiPath.equals(API_CORPORATION)) {
+            return corporationService.findAllAuthEntityData();
+        }
         //调用API服务，获取业务实体
         String path = String.format("%s/%s", apiPath, FIND_ALL_AUTH_ENTITY_DATA_METHOD);
         ParameterizedTypeReference<ResultData<List<AuthEntityData>>> typeReference = new ParameterizedTypeReference<ResultData<List<AuthEntityData>>>() {};
@@ -70,6 +86,10 @@ public class DataAuthManager {
      * @return 数据权限值清单
      */
     public List<AuthTreeEntityData> getAuthTreeEntityDataByIds(String appModuleCode, String apiPath, List<String> entityIds){
+        // 判断是组织机构,直接执行组织机构业务逻辑
+        if (apiPath.equals(API_ORGANIZATION)) {
+            return organizationService.getAuthTreeEntityDataByIds(entityIds);
+        }
         //调用API服务，获取业务实体
         String path = String.format("%s/%s", apiPath, GET_AUTH_TREE_ENTITY_DATA_METHOD);
         ParameterizedTypeReference<ResultData<List<AuthTreeEntityData>>> typeReference = new ParameterizedTypeReference<ResultData<List<AuthTreeEntityData>>>() {};
@@ -87,6 +107,10 @@ public class DataAuthManager {
      * @return 数据权限值清单
      */
     public List<AuthTreeEntityData> findAllAuthTreeEntityData(String appModuleCode, String apiPath){
+        // 判断是组织机构,直接执行组织机构业务逻辑
+        if (apiPath.equals(API_ORGANIZATION)) {
+            return organizationService.findAllAuthTreeEntityData();
+        }
         //调用API服务，获取业务实体
         String path = String.format("%s/%s", apiPath, FIND_ALL_AUTH_TREE_ENTITY_DATA_METHOD);
         ParameterizedTypeReference<ResultData<List<AuthTreeEntityData>>> typeReference = new ParameterizedTypeReference<ResultData<List<AuthTreeEntityData>>>() {};
