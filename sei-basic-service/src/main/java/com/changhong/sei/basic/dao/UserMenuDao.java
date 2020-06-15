316aa3f34ffc5de852c78bc879ead4f7cf85ffdf
王锦光 <wangjg@changhong.com>
2020-06-15 14:12:42 +0800

    获取当前用户收藏的菜单清单按创建时间降序:VERSION = 6.0.1.148

--- sei-basic-service/src/main/java/com/changhong/sei/basic/dao/UserMenuDao.java
+++ sei-basic-service/src/main/java/com/changhong/sei/basic/dao/UserMenuDao.java
@@ -4,11 +4,23 @@ import com.changhong.sei.basic.entity.Menu;
 import com.changhong.sei.basic.entity.User;
 import com.changhong.sei.basic.entity.UserMenu;
 import com.changhong.sei.core.dao.BaseRelationDao;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * 实现功能： 用户收藏的菜单数据访问接口
  */
 @Repository
 public interface UserMenuDao extends BaseRelationDao<UserMenu, User, Menu> {
+    /**
+     * 通过父实体Id获取子实体清单
+     *
+     * @param parentId 父实体Id
+     * @return 子实体清单
+     */
+    @Override
+    @Query("select r.child from UserMenu r where r.parent.id=?1 order by r.createdDate desc ")
+    List<Menu> getChildrenFromParentId(String parentId);
 }
