ebcc4286e3139eb1b05795aee4e9d9b96a446493
Vision.Mac <chao2.ma@changhong.com>
2021-01-25 23:56:54 +0800

    优化版本管理

--- sei-basic-api/src/main/java/com/changhong/sei/basic/BasicVersion.java
+++ sei-basic-api/src/main/java/com/changhong/sei/basic/BasicVersion.java
@@ -1,6 +1,6 @@
 package com.changhong.sei.basic;
 
-import com.changhong.sei.core.context.Version;
+import com.changhong.sei.core.BaseVersion;
 
 /**
  * 实现功能：应用版本
@@ -8,7 +8,7 @@ import com.changhong.sei.core.context.Version;
  * @author 马超(Vision.Mac)
  * @version 1.0.00  2021-01-04 15:34
  */
-public class BasicVersion extends Version {
+public class BasicVersion extends BaseVersion {
     public BasicVersion() {
         super(BasicVersion.class);
     }
---
+++ sei-basic-sdk/src/main/java/com/changhong/sei/basic/BasicVersion.java
@@ -0,0 +1,15 @@
+package com.changhong.sei.basic;
+
+import com.changhong.sei.core.BaseVersion;
+
+/**
+ * 实现功能：应用版本
+ *
+ * @author 马超(Vision.Mac)
+ * @version 1.0.00  2021-01-04 15:34
+ */
+public class BasicVersion extends BaseVersion {
+    public BasicVersion() {
+        super(BasicVersion.class);
+    }
+}
